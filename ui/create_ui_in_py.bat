bf6086a680693e555bd95508de998a669ee76be2
Pavel Zagumenkin <pavelzagumenkin@gmail.com>
2024-07-23 10:31:08 +0400

    Удалил Мичурина из списка для пирожных, добавил ГорПарк в список для приоритетов

--- ui/create_ui_in_py.bat
+++ ui/create_ui_in_py.bat
@@ -1,6 +1,6 @@
 @REM pyuic6 -x login.ui -o login.py
 @REM pyuic6 -x viborRazdela.ui -o viborRazdela.py
-pyuic6 -x bakery.ui -o bakery.py
+@REM pyuic6 -x bakery.ui -o bakery.py
 pyuic6 -x pie.ui -o pie.py
 pyuic6 -x DialogPrioritet.ui -o DialogPrioritet.py
 @REM pyuic6 -x cake.ui -o cake.py
