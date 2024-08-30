9e837578f7296fbc8b43aaab29a3783a617fb333
Pavel Zagumenkin <pavelzagumenkin@gmail.com>
2024-08-30 09:07:41 +0400

    Добавлена точка Центр

--- ui/create_ui_in_py.bat
+++ ui/create_ui_in_py.bat
@@ -1,8 +1,8 @@
 @REM pyuic6 -x login.ui -o login.py
 @REM pyuic6 -x viborRazdela.ui -o viborRazdela.py
-@REM pyuic6 -x bakery.ui -o bakery.py
-pyuic6 -x pie.ui -o pie.py
-pyuic6 -x DialogPrioritet.ui -o DialogPrioritet.py
+pyuic6 -x bakery.ui -o bakery.py
+@REMpyuic6 -x pie.ui -o pie.py
+@REMpyuic6 -x DialogPrioritet.ui -o DialogPrioritet.py
 @REM pyuic6 -x cake.ui -o cake.py
 @REM pyuic6 -x filling.ui -o filling.py
 @REM pyuic6 -x bakeryTables.ui -o bakeryTables.py
